3082c65238eac3368a32d98c93c29b92dd7e88b1
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2021-07-19 01:30:46 +0400

    add company controller

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CompanyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Json("Index");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Show(int id)
+        {
+            return Json("Show");
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Json("Create");
+        }
+
+        [HttpPut]
+        public IActionResult Update(int id)
+        {
+            return View();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
